a05a4542d870c8148341637f016202aec9e63db7
anky <iqoptionankit@gmail.com>
2021-10-02 16:41:38 +0530

    completed the fb_commentBox

--- script.js
+++ script.js
@@ -1,22 +1,91 @@
-let myButton = document.querySelector("button");
-let myVal = document.querySelector("textarea");
-let contentArea = document.getElementById("content");
+let myTextInput = document.querySelector("textarea");
 
-let addComment = () => {
-  let myNewVal = document.createElement("p");
-  let myImg = document.createElement("img");
+let apiImgArea = document.getElementById("for-api-img");
 
-  // myNewVal.style.margin = "10px";
-  // myNewVal.style.border = "1px solid";
-  // myNewVal.style.textAlign = "left";
-  myNewVal.classList.add("formate-comments");
+let gifAddButton = document.getElementById("fetch-gif");
 
-  myNewVal.innerText = myVal.value;
+let commentButton = document.querySelector("button");
 
-  myImg.src = "todo's.svg";
+let commentContent = document.getElementById("content");
 
-  contentArea.appendChild(myNewVal);
-  contentArea.appendChild(myImg);
+//action definations
+
+//
+let fetchImgUi = () => {
+  let searchInptGify = document.createElement("input");
+
+  let searchButton = document.createElement("button");
+
+  let searchBoxGify = document.createElement("div");
+
+  searchButton.classList.add("searchButton");
+  searchButton.innerText = "search";
+
+  searchInptGify.classList.add("searchInputGify");
+
+  searchBoxGify.classList.add("searchBoxGify");
+
+  apiImgArea.appendChild(searchInptGify);
+  apiImgArea.appendChild(searchButton);
+
+  //calling rest API
+  let fetchImg = () => {
+    let apiKey = "LHV86MP6aMHyhYiOLEajf0eiQ1EzgZu4";
+
+    let searchInput = searchInptGify.value.trim();
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=1&q=`;
+    url = url.concat(searchInput);
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((content) => {
+        // console.log(content.data);
+        let img = document.createElement("img");
+        img.src = content.data[0].images.downsized.url;
+
+        // img.style.contenteditable = "true";
+
+        apiImgArea.appendChild(img);
+
+        apiImgArea.removeChild(searchInptGify);
+        apiImgArea.removeChild(searchButton);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  searchButton.addEventListener("click", fetchImg);
 };
 
-myButton.addEventListener("click", addComment);
+//adding comments
+let addComment = () => {
+  let myImgUrl = document.querySelector("img");
+  if (myTextInput.value != "" && myImgUrl.src != null) {
+    let divComments = document.createElement("div");
+    let myTextComment = document.createElement("p");
+    let myImgComment = document.createElement("img");
+
+    // let myImgUrl = document.querySelector("img");
+
+    myTextComment.innerText = myTextInput.value;
+
+    myImgComment.src = myImgUrl.src;
+
+    divComments.appendChild(myTextComment);
+    divComments.appendChild(myImgComment);
+    commentContent.appendChild(divComments);
+  }
+
+  //clearing comment input area
+  myTextInput.value = "";
+
+  while (apiImgArea.firstChild) {
+    apiImgArea.removeChild(apiImgArea.firstChild);
+  }
+};
+
+//action buttons
+commentButton.addEventListener("click", addComment);
+
+gifAddButton.addEventListener("click", fetchImgUi);
